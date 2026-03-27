0e1fb14a3e1d1e73700eb262e9b81a981590a6d9
ismandd <is.mand@icloud.com>
2026-03-27 17:16:02 +0100

    cloudflare workers

--- nitro.config.ts
+++ nitro.config.ts
@@ -1,22 +1,32 @@
 import { config } from 'dotenv';
 config();
+
 import { version } from './server/utils/config';
-//https://nitro.unjs.io/config
+
 export default defineNitroConfig({
   srcDir: 'server',
   compatibilityDate: '2025-03-05',
+
   experimental: {
     asyncContext: true,
     tasks: true,
   },
+
+  preset: 'cloudflare_module',
+  cloudflare: {
+    deployConfig: true,    // generate wrangler.json for you
+    nodeCompat: true,      // enables Node‑style polyfills (recommended for Node‑like libs)
+  },
+
   scheduledTasks: {
     // Daily cron jobs (midnight)
     '0 0 * * *': ['jobs:clear-metrics:daily'],
     // Weekly cron jobs (Sunday midnight)
     '0 0 * * 0': ['jobs:clear-metrics:weekly'],
     // Monthly cron jobs (1st of month at midnight)
-    '0 0 1 * *': ['jobs:clear-metrics:monthly']
+    '0 0 1 * *': ['jobs:clear-metrics:monthly'],
   },
+
   runtimeConfig: {
     public: {
       meta: {
